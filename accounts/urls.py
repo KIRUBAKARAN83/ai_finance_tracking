1949b2bdbf8b89f22b07fd946b545f2fe5e45202
D.KIRUBAKARAN <dhayalankiruba17@gmail.com>
2026-01-07 18:55:14 +0530

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import register, profile
 
+app_name = "accounts"   # <-- add this for namespacing
+
 urlpatterns = [
     path("register/", register, name="register"),
     path("profile/", profile, name="profile"),
