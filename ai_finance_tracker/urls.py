0bb0fd441054c5d35b671a69cb10dc4227ebdce8
D.KIRUBAKARAN <dhayalankiruba17@gmail.com>
2026-01-07 16:19:34 +0530

    Update urls.py

--- ai_finance_tracker/urls.py
+++ ai_finance_tracker/urls.py
@@ -1,12 +1,10 @@
+
 from django.contrib import admin
 from django.urls import path, include
-from transactions import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("accounts/", include("django.contrib.auth.urls")),
     path("accounts/", include("accounts.urls")),
-    path("", include("transactions.urls")),  # MUST EXIST
-    path("transactions/", views.all_transactions, name="all_transactions"),
-
+    path("transactions/", include(("transactions.urls", "transactions"), namespace="transactions")),
+    path("insights/", include("insights.urls")),
 ]
