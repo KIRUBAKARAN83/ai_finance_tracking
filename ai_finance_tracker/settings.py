59b159446d854e79edc1b5bcccd25cf9780e2b72
D.KIRUBAKARAN <dhayalankiruba17@gmail.com>
2026-01-08 16:45:39 +0530

    Update settings.py

--- ai_finance_tracker/settings.py
+++ ai_finance_tracker/settings.py
@@ -146,10 +146,9 @@ STATICFILES_STORAGE = (
 # =================================================
 # AUTH / ALLAUTH
 # =================================================
-LOGIN_URL = "login"
+LOGIN_URL = "/accounts/login/"
 LOGIN_REDIRECT_URL = "/"
-LOGOUT_REDIRECT_URL = "login"
-
+LOGOUT_REDIRECT_URL = "/accounts/login/"
 
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = "none"
