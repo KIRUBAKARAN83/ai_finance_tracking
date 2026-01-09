448578154c681cc95fb36a49317fff134a50b5b0
D.KIRUBAKARAN <dhayalankiruba17@gmail.com>
2026-01-09 20:55:58 +0530

    Update urls.py

--- ai_finance_tracker/urls.py
+++ ai_finance_tracker/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     # Authentication (Django + Allauth)
    
     path("accounts/", include("accounts.urls")),
+     path("accounts/", include("django.contrib.auth.urls")),
 
     # Transactions app (dashboard at /)
     path("", include("transactions.urls")),
