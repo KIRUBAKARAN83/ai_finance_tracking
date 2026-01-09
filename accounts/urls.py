62139826df161a74f8bb709e12f459d196401c64
D.KIRUBAKARAN <dhayalankiruba17@gmail.com>
2026-01-09 20:27:30 +0530

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,8 +5,7 @@ from .views import register, profile
 urlpatterns = [
     path("register/", register, name="register"),
 
-    # ✅ ALLAUTH HANDLES LOGIN
-    path("", include("allauth.urls")),
+
 
     path("profile/", profile, name="profile"),
 ]
