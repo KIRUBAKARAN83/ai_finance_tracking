933f489c9c6e4cac2bf4bb6d5286f1be342e400a
D.KIRUBAKARAN <dhayalankiruba17@gmail.com>
2026-01-09 23:11:07 +0530

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
 
     # Login and logout using Django built-in views
     path("login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),s
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
