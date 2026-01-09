03e8f88d06526c9129e19c286de9514d40c6549f
D.KIRUBAKARAN <dhayalankiruba17@gmail.com>
2026-01-09 22:34:08 +0530

    Update admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,13 +1,20 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, UserActivity
 
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = (
-        'user',
-        'monthly_income',
+        "user",
+        "monthly_income",
     )
     search_fields = (
-        'user__username',
+        "user__username",
     )
+
+
+@admin.register(UserActivity)
+class UserActivityAdmin(admin.ModelAdmin):
+    list_display = ("user", "last_seen")
+    search_fields = ("user__username",)
+    readonly_fields = ("last_seen",)
