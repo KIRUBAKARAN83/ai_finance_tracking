7939c934556dcdf9ad504424785d5d1b26c430a3
D.KIRUBAKARAN <dhayalankiruba17@gmail.com>
2026-01-09 22:07:21 +0530

    Update urls.py

--- ai_finance_tracker/urls.py
+++ ai_finance_tracker/urls.py
@@ -2,15 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    # Admin
+    # Admin site
     path("admin/", admin.site.urls),
 
-    # Authentication (Django + Allauth)
-   
-    path("accounts/", include("accounts.urls")),
-     path("accounts/", include("django.contrib.auth.urls")),
+    # Authentication (custom + built-in)
+    path("accounts/", include(("accounts.urls", "accounts"), namespace="accounts")),
+    path("accounts/", include("django.contrib.auth.urls")),
 
     # Transactions app (dashboard at /)
     path("", include(("transactions.urls", "transactions"), namespace="transactions")),
-
 ]
