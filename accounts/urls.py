71eb9ff5b519019b2b37fe84f1d313c4a916c518
D.KIRUBAKARAN <dhayalankiruba17@gmail.com>
2026-01-10 20:49:27 +0530

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,3 +10,26 @@ urlpatterns = [
     # Login and logout using Django built-in views
    ]
 
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from .views import register, profile
+
+app_name = "accounts"
+
+urlpatterns = [
+    path("register/", register, name="register"),
+    path("profile/", profile, name="profile"),
+
+    # Login and logout using Django built-in views
+    path(
+        "login/",
+        auth_views.LoginView.as_view(template_name="templates/login.html"),
+        name="login",
+    ),
+    path(
+        "logout/",
+        auth_views.LogoutView.as_view(next_page="accounts:login"),
+        name="logout",
+    ),
+
+]
