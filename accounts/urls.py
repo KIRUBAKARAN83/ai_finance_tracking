300d87ce1d16bab5b30da8a2dc7c4234b9456213
D.KIRUBAKARAN <dhayalankiruba17@gmail.com>
2026-01-10 20:50:01 +0530

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,14 +1,4 @@
-from django.urls import path
-from django.contrib.auth import views as auth_views
-from .views import register, profile  # keep your custom views
-app_name = "accounts"   # <-- add this for namespacing
 
-urlpatterns = [
-    path("register/", register, name="register"),
-    path("profile/", profile, name="profile"),
-
-    # Login and logout using Django built-in views
-   ]
 
 from django.urls import path
 from django.contrib.auth import views as auth_views
