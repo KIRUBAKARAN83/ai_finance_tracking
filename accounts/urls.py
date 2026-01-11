6b3d7a991b59add161687d16fbc923fd26f30d84
D.KIRUBAKARAN <dhayalankiruba17@gmail.com>
2026-01-11 14:55:22 +0530

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 
 
+# accounts/urls.py
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from .views import register, profile
@@ -7,7 +8,20 @@ from .views import register, profile
 app_name = "accounts"
 
 urlpatterns = [
+    # Custom views
     path("register/", register, name="register"),
-    path("profile/", profile, name="profile")
+    path("profile/", profile, name="profile"),
 
+    # Built-in authentication views
+    path(
+        "login/",
+        auth_views.LoginView.as_view(template_name="accounts/login.html"),
+        name="login",
+    ),
+    path(
+        "logout/",
+        auth_views.LogoutView.as_view(next_page="accounts:login"),
+        name="logout",
+    ),
 ]
+
