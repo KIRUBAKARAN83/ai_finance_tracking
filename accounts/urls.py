83c6a543194681fb135e1e63b6e79690cd3b1cbb
D.KIRUBAKARAN <dhayalankiruba17@gmail.com>
2026-01-11 12:34:28 +0530

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,18 +8,6 @@ app_name = "accounts"
 
 urlpatterns = [
     path("register/", register, name="register"),
-    path("profile/", profile, name="profile"),
-
-    # Login and logout using Django built-in views
-    path(
-        "login/",
-        auth_views.LoginView.as_view(template_name="accounts/login.html"),
-        name="login",
-    ),
-    path(
-        "logout/",
-        auth_views.LogoutView.as_view(next_page="accounts:login"),
-        name="logout",
-    ),
+    path("profile/", profile, name="profile")
 
 ]
